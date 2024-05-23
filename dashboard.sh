8229fc2c82baeeabb80f13f08000d2ad320b26ec
Harshil323 <harshil.7652.gandhi@gmail.com>
2024-05-23 19:45:32 +0000

    This is the shell script to list the AWS usage

---
+++ dashboard.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#######################################
+#
+#Author : harshil
+#Date   : 20th May 2024
+#Verion : v1
+#
+#This script will report the AWS resource Usage
+#######################################
+
+set -x
+
+echo "List the list of s3 buckets"
+aws s3 ls > output.txt
+
+echo "List the list of Lambda function"
+aws lambda list-functions >> output.txt
+
+echo "List of EC2 instances"
+aws ec2 describe-instances >> output.txt
+
+echo "List of IAM Users"
+aws iam list-users | jq '.Users[].UserName' >> output.txt
+echo "List of EBS volumns in cloud"
